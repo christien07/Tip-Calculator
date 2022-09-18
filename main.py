8ade046230b910597b6546ec8e50d52a3b10027e
christien07 <christien0101@protonmail.com>
2022-09-17 20:12:35 -0700

    Update README.md

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def main():
     print("Welcome to the tip calculator.")
     total_bill = input("What was your total bill?\n$")
     tip = input("What percentage tip would you like to give? 10, 12, or 15 percent? ")
-    print(round(float(total_bill) * (int(tip) / 100)))
+    print("Your total tip amount is about $" + str(round(float(total_bill) * (int(tip) / 100))))
 
 if __name__ == "__main__":
     main()
