1ca59ff85b6e33d788f24df986363c73b6e3c712
christien07 <christien0101@protonmail.com>
2022-09-17 20:01:55 -0700

    Add calculation for tip amount

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def main():
     print("Welcome to the tip calculator.")
-    input("What was your total bill?\n$")
+    total_bill = input("What was your total bill?\n$")
+    tip = input("What percentage tip would you like to give? 10, 12, or 15 percent? ")
 
 if __name__ == "__main__":
     main()
